d662389c9b0e719e6518d7230410c3f4290c2fdf
Derek Williams <williamsdj503@gmail.com>
2024-09-05 10:05:40 -0600

    I learned how to count the amount of lines in a .txt file using Python.

--- line_count.py
+++ line_count.py
@@ -1,8 +1,13 @@
 def line_count():
   count = 0 
+# Start "count" at 0 since we'll be adding values to it for each line in the .txt file.            
   file = open('file.txt', 'r')
+# Open the file and use the mode 'r' to read the file without making any changes.
   lines = file.readlines()
+# Iterate over each line in file.
   for line in lines:
     count = count + 1 
+# For each new line update the variable "count" by 1.
   file.close()
   return count
+# When returning variable "count" make sure the variable is outside the loop to return the last value (the total amount of lines in the file).
